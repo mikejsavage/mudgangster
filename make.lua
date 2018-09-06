d508237d1b0aa81d33379e28d6da3f6dc082501f
Michael Savage <mikejsavage@gmail.com>
2018-09-06 13:30:41 +0300

    make.lua cleanup

--- make.lua
+++ make.lua
@@ -1,9 +1,6 @@
 require( "scripts.gen_makefile" )
 
--- bin( "wintest", { "win", "platform_network", "ggformat", "strlcpy", "strlcat", "patterns", "strtonum" } )
--- msvc_bin_ldflags( "wintest", "gdi32.lib Ws2_32.lib" )
-
-local main_obj = { "src/main", "src/x11" }
+local platform_objs = { "src/main", "src/x11" }
 local libs = { }
 
 if OS == "windows" then
@@ -11,10 +8,10 @@ if OS == "windows" then
 	require( "libs.lpeg" )
 	require( "libs.lfs" )
 
-	main_obj = "src/win32"
+	platform_objs = "src/win32"
 	libs = { "lua", "lpeg", "lfs" }
 end
 
-bin( "mudgangster", { main_obj, "src/script", "src/textbox", "src/input", "src/platform_network" }, libs )
+bin( "mudgangster", { platform_objs, "src/script", "src/textbox", "src/input", "src/platform_network" }, libs )
 msvc_bin_ldflags( "mudgangster", "gdi32.lib Ws2_32.lib" )
-gcc_bin_ldflags( "mudgangster", "-lm -lX11 -llua" ) -- -Wl,-E" ) need to export symbols when vendoring
+gcc_bin_ldflags( "mudgangster", "-lm -lX11 -llua" )

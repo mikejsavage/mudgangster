bd0ff6bd2213fc11e8455d1d6d03a51a99d43570
Michael Savage <mikejsavage@gmail.com>
2018-09-07 15:20:15 +0300

    Smaller scrollback

--- src/textbox.h
+++ src/textbox.h
@@ -3,7 +3,7 @@
 #include "ui.h"
 
 constexpr size_t MAX_LINE_LENGTH = 2048;
-constexpr size_t SCROLLBACK_SIZE = 1 << 16;
+constexpr size_t SCROLLBACK_SIZE = 1 << 14;
 
 struct TextBox {
 	struct Glyph {
